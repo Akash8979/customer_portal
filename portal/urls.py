847c6ae2ceb110456cedb7c561a63f0a96f92fca
akash.kumar1 <akash.kumar1@3scsolution.com>
2026-04-07 16:52:40 +0530

    changes

--- portal/urls.py
+++ portal/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import (
-    TicketCreateView, TicketDetailView, TicketUpdateView,
+    TicketCreateView, TicketListView, TicketDetailView, TicketUpdateView,
     TicketAttachmentView, CommentCreateView, CommentUpdateView,
 )
 
 urlpatterns = [
     path('tickets', TicketCreateView.as_view(), name='ticket-create'),
+    path('tickets/list', TicketListView.as_view(), name='ticket-list'),
     path('tickets/<int:pk>', TicketDetailView.as_view(), name='ticket-detail'),
     path('tickets/<int:pk>/update', TicketUpdateView.as_view(), name='ticket-update'),
     path('attachments', TicketAttachmentView.as_view(), name='ticket-attachments'),
